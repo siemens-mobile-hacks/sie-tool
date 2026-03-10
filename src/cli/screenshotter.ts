0ede9d44b23072c1974d5ea7ae1b89ac2c50c627
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2026-03-10 21:07:21 +0000

    update deps

--- src/cli/screenshotter.ts
+++ src/cli/screenshotter.ts
@@ -6,7 +6,13 @@ import { getBitmapDecoder } from "#src/utils/bitmap.js";
 import fs from "node:fs";
 import { BfcDisplayBufferData } from "@sie-js/serial";
 import { DateTime } from "luxon";
-import { JimpInstance, Jimp } from "jimp";
+import { Jimp } from "jimp";
+
+interface BfcDisplayDecodedBuffer {
+	width: number;
+	height: number;
+	data: Buffer;
+}
 
 export interface CLIMakeScreenshotOptions extends CLIBaseOptions {
 	display?: string;
@@ -43,7 +49,12 @@ export const cliMakeScreenshot: AppCommand<CLIMakeScreenshotOptions> = async (op
 	const outputFilename = options.output && isDir(options.output) ?
 		`${options.output}/${defaultFilename}` : options.output ?? defaultFilename;
 
-	const image = createImageFromBuffer(response);
+	const decodedImage = decodeBfcDisplayBuffer(response);
+	const image = new Jimp({
+		width: decodedImage.width,
+		height: decodedImage.height,
+	});
+	image.bitmap.data.set(decodedImage.data);
 	console.log(`Saving screenshot to ${outputFilename}`);
 	await image.write(outputFilename as `${string}.${string}`);
 }
@@ -56,37 +67,75 @@ function isDir(path: string) {
 	return false;
 }
 
-function createImageFromBuffer(response: BfcDisplayBufferData): JimpInstance {
+export function decodeBfcDisplayBuffer(response: BfcDisplayBufferData): BfcDisplayDecodedBuffer {
 	let type = response.type;
-	let isMask = false;
-	if (type == 'bgra8888mask') {
+	let isYuvMask = false;
+	if (type == 'bgra8888+yuv') {
 		type = 'bgra8888';
-		isMask = true;
+		isYuvMask = true;
 	}
 
-	const image = new Jimp({
-		width: response.width,
-		height: response.height,
-	});
+	const clamp8 = (v: number): number => {
+		return v < 0 ? 0 : v > 255 ? 255 : v;
+	};
 
+	const decodeYUV = (y: number, u: number, v: number): number => {
+		const d = u - 128;
+		const e = v - 128;
+		const r = clamp8((298 * y + 409 * e + 128) >> 8);
+		const g = clamp8((298 * y - 100 * d - 208 * e + 128) >> 8);
+		const b = clamp8((298 * y + 516 * d + 128) >> 8);
+		return 0xFF000000 | (b << 16) | (g << 8) | r;
+	};
+
+	const image: BfcDisplayDecodedBuffer = {
+		width: response.displayWidth,
+		height: response.displayHeight,
+		data: Buffer.alloc(response.displayWidth * response.displayHeight * 4),
+	};
 	const getBitmapPixel = getBitmapDecoder(type);
 	for (let y = 0; y < response.height; y++) {
 		for (let x = 0; x < response.width; x++) {
 			let color = getBitmapPixel(x, y, response.width, response.height, response.buffer);
-			if (isMask) { // EL71 in camera
+			if (isYuvMask) { // EL71 in camera
 				const mask = (color & 0xFF000000) >>> 24;
-				color = mask == 0x8D ? 0xFF00FF00 : (color | 0xFF000000) >>> 0;
+				if (mask == 0x8D) {
+					color = decodeYUV(color & 0xFF, (color >>> 8) & 0xFF, (color >>> 16) & 0xFF) >>> 0;
+				} else {
+					color = (color | 0xFF000000) >>> 0;
+				}
 			}
-			const offset = (y * image.bitmap.width + x) * 4;
-			image.bitmap.data.writeUInt32LE(color, offset);
+			image.data.writeUInt32LE(color, (y * image.width + x) * 4);
 		}
 	}
+
+	const cropImage = (x: number, y: number, w: number, h: number): BfcDisplayDecodedBuffer => {
+		const out: BfcDisplayDecodedBuffer = {
+			width: w,
+			height: h,
+			data: Buffer.alloc(w * h * 4),
+		};
+
+		const src = image.data;
+		const dst = out.data;
+
+		const stride = image.width * 4;
+		const bytesPerRow = w * 4;
+
+		for (let row = 0; row < h; row++) {
+			const offsetSrc = (y + row) * stride + x * 4;
+			const offsetDst = row * bytesPerRow;
+			src.copy(dst, offsetDst, offsetSrc, offsetSrc + bytesPerRow);
+		}
+
+		return out;
+	};
 
 	// C72 shit
 	if (response.displayWidth < response.width || response.displayHeight < response.height) {
 		const x = Math.round((response.width - response.displayWidth) / 2);
 		const y = Math.round((response.height - response.displayHeight) / 2);
-		return image.crop({ x, y, w: response.displayWidth, h: response.displayHeight }) as JimpInstance;
+		return cropImage(x, y, response.displayWidth, response.displayHeight);
 	}
 
 	return image;
